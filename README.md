7740b6e01040bc871086572acbfc046cf30ae6f0
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-11-18 15:31:16 +0100

    add docs

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# dmb-tsys-spring-1611-ta27
+<div align="center"><img width="80%"  src="https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2010-ta15/blob/main/docs/logoDark.png?raw=trueg"  alt="Logo BootCamp" /></div>
+
+# BootCamp T-Systems
+
+![My etiqueta](https://img.shields.io/badge/David%20Maza-DiveCode%F0%9F%90%99-blue) ![UD](https://img.shields.io/badge/TA-27-orange)  ![Java Version](https://img.shields.io/badge/SprintBoot-Raillway-red)
+
+# T27 - T27 - Spring REST JWT
+
+Este repositorio contiene las tareas del Bootcamp 👨‍💻
+
+#### ### Rules of use 🤜🏼🤛🏼
+
+- Run the clone repository in your terminal:
+
+``git clone https://github.com/d-maza/dmb-tsys-spring-1611-ta27``
+
+
+- Access the folder with the command:
+
+`cd dmb-tsys-spring-1611-ta27 `
+
+- Open the repository with your IDE, remember to have the SQL service running
+
+- Update the dependencies and remember to create the databases
+
---
+++ docs/js/lessons.js
@@ -0,0 +1,232 @@
+export const lessons = [
+  {
+    numLessons: 4,
+    issue: 'SpringBoot Security & JWT',
+    day: 49, // inicio 27 de Septiembre
+    msg: 'La programación web segura es una inversión que protege a los usuarios y a las empresas.',
+    myProgress: '🥚>🐣>🐤>🐓>🕊️>🦃>🦅>🪂>🛩️>✈️>👨‍🚀>🚀>🛸>🦸‍♂️',
+
+    exercise: [
+      {
+        numExercise: 1,
+        title: '',
+        description: `<small> <li> <b> PASO 1 </b>: Registrate en POST /user pasando el user y la password  <a href="http://">ejemplo</a></li>
+        <li> <b> PASO 2 </b>: Logeate en POST /login con la user y la password que has pasado enteriormente   <a href="http://">ejemplo</a></li> 
+        <li> <b> PASO 3 </b>: Una vez obtenido tu TOKEN que da accaeso a la API ya puedes accedes a las direrentes rutas colocando el TOKEN en el header de la perticon http o https  <a href="http://">ejemplo</a></li> 
+        <li> <b> PASO 4 </b>: Ya puedes acceder a los diferentes endpoints..  <a href="http://">ejemplo</a></li> 
+        <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ENTITY</th>
+            <th>GET ALL</th>
+            <th>GET ONE</th>
+            <th>POST</th>
+            <th>PUT</th>
+            <th>DELETE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <th scope="row">PIEZAS</th>
+          <td>GET /api/v1/piezas</td>
+          <td>GET /api/v1/pieza/{id}</td>
+          <td>POST /api/v1/pieza</td>
+          <td>PUT /api/v1/pieza/{id}</td>
+          <td>DELETE /api/v1/pieza/{id}</td>
+  
+          </tr>
+          <tr>
+          <th scope="row">PROVEEDOR</th>
+          <td>GET /api/v1/proveedores</td>
+          <td>GET /api/v1/proveedor/{id}</td>
+          <td>POST /api/v1/proveedor</td>
+          <td>PUT /api/v1/proveedor/{id}</td>
+          <td>DELETE /api/v1/proveedor/{id}</td>
+          </tr>
+          <tr>
+          <th scope="row">SUMINISTRA</th>
+          <td>GET /api/v1/suministras</td>
+          <td>GET /api/v1/suministra/{id}</td>
+          <td>POST /api/v1/suministra</td>
+          <td>PUT /api/v1/suministra/{id}</td>
+          <td>DELETE /api/v1/suministra/{id}</td>
+          </tr>
+        </tbody>
+      </table> 
+       </small>`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-spring-1611-ta27/tree/main/TA27_1',
+        urlPG: '',
+      },
+      {
+        numExercise: 2,
+        title: '',
+        description: `<small> <li> <b> PASO 1 </b>: Registrate en POST /user pasando el user y la password  <a href="http://">ejemplo</a></li>
+        <li> <b> PASO 2 </b>: Logeate en POST /login con la user y la password que has pasado enteriormente   <a href="http://">ejemplo</a></li> 
+        <li> <b> PASO 3 </b>: Una vez obtenido tu TOKEN que da accaeso a la API ya puedes accedes a las direrentes rutas colocando el TOKEN en el header de la perticon http o https  <a href="http://">ejemplo</a></li> 
+        <li> <b> PASO 4 </b>: Ya puedes acceder a los diferentes endpoints..  <a href="http://">ejemplo</a></li> 
+        <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ENTITY</th>
+            <th>GET ALL</th>
+            <th>GET ONE</th>
+            <th>POST</th>
+            <th>PUT</th>
+            <th>DELETE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <th scope="row">CIENTIFICO</th>
+          <td>GET /api/v1/cientificos</td>
+          <td>GET /api/v1/cientifico/{id}</td>
+          <td>POST /api/v1/cientifico</td>
+          <td>PUT /api/v1/cientifico/{id}</td>
+          <td>DELETE /api/v1/cientifico/{id}</td>
+  
+          </tr>
+          <tr>
+          <th scope="row">PROYECTOS</th>
+          <td>GET /api/v1/proyectos</td>
+          <td>GET /api/v1/proyecto/{id}</td>
+          <td>POST /api/v1/proyecto</td>
+          <td>PUT /api/v1/proyecto/{id}</td>
+          <td>DELETE /api/v1/proyecto/{id}</td>
+          </tr>
+        </tbody>
+      </table> 
+       </small>`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-spring-1611-ta27/tree/main/TA27_2',
+        urlPG: '',
+      },
+      {
+        numExercise: 3,
+        title: '',
+        description: `<small> <li> <b> PASO 1 </b>: Registrate en POST /user pasando el user y la password  <a href="http://">ejemplo</a></li>
+        <li> <b> PASO 2 </b>: Logeate en POST /login con la user y la password que has pasado enteriormente   <a href="http://">ejemplo</a></li> 
+        <li> <b> PASO 3 </b>: Una vez obtenido tu TOKEN que da accaeso a la API ya puedes accedes a las direrentes rutas colocando el TOKEN en el header de la perticon http o https  <a href="http://">ejemplo</a></li> 
+        <li> <b> PASO 4 </b>: Ya puedes acceder a los diferentes endpoints..  <a href="http://">ejemplo</a></li> 
+        <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ENTITY</th>
+            <th>GET ALL</th>
+            <th>GET ONE</th>
+            <th>POST</th>
+            <th>PUT</th>
+            <th>DELETE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+     
+            <th scope="row">CAJERO</th>
+          
+            <td>GET /api/v1/cajeros</td>
+            <td>GET /api/v1/cajero/{id}</td>
+            <td>POST /api/v1/cajero</td>
+            <td>PUT /api/v1/cajero/{id}</td>
+            <td>DELETE /api/v1/cajero/{id}</td>
+          </tr>
+          <tr>
+            <th scope="row"> MAQUINA_REG </th>
+        
+            <td>GET /api/v1/maquina-registradoras</td>
+            <td>GET /api/v1/maquina-registradora/{id}</td>
+            <td>POST /api/v1/maquina-registradora</td>
+            <td>PUT /api/v1/maquina-registradora/{id}</td>
+            <td>DELETE /api/v1/maquina-registradora/{id}</td>
+          </tr>
+          <tr>
+            <th scope="row"> VENTA </th>
+        
+            <td>GET /api/v1/ventas</td>
+            <td>GET /api/v1/venta/{id}</td>
+            <td>POST /api/v1/venta</td>
+            <td>PUT /api/v1/venta/{id}</td>
+            <td>DELETE /api/v1/venta/{id}</td>
+          </tr>
+          <tr>
+            <th scope="row"> RODUCTO </th>
+            
+            <td>GET /api/v1/productos</td>
+            <td>GET /api/v1/producto/{id}</td>
+            <td>POST /api/v1/producto</td>
+            <td>PUT /api/v1/producto/{id}</td>
+            <td>DELETE /api/v1/producto/{id}</td>
+          </tr>
+        </tbody>
+      </table> 
+       </small>`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-spring-1611-ta27/tree/main/TA27_3',
+        urlPG: '',
+      },
+      {
+        numExercise: 4,
+        title: '',
+        description: `<small> <li> <b> PASO 1 </b>: Registrate en POST /user pasando el user y la password  <a href="http://">ejemplo</a></li>
+        <li> <b> PASO 2 </b>: Logeate en POST /login con la user y la password que has pasado enteriormente   <a href="http://">ejemplo</a></li> 
+        <li> <b> PASO 3 </b>: Una vez obtenido tu TOKEN que da accaeso a la API ya puedes accedes a las direrentes rutas colocando el TOKEN en el header de la perticon http o https  <a href="http://">ejemplo</a></li> 
+        <li> <b> PASO 4 </b>: Ya puedes acceder a los diferentes endpoints..  <a href="http://">ejemplo</a></li> 
+        <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ENTITY</th>
+            <th>GET ALL</th>
+            <th>GET ONE</th>
+            <th>POST</th>
+            <th>PUT</th>
+            <th>DELETE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+     
+            <th scope="row">EQUIPOS</th>
+          
+            <td>GET /api/v1/equiposs</td>
+            <td>GET /api/v1/equipo/{id}</td>
+            <td>POST /api/v1/equipo</td>
+            <td>PUT /api/v1/equipo/{id}</td>
+            <td>DELETE /api/v1/equipo/{id}</td>
+          </tr>
+          <tr>
+            <th scope="row"> FACULTAD</th>
+        
+            <td>GET /api/v1/facultades</td>
+            <td>GET /api/v1/facultad/{id}</td>
+            <td>POST /api/v1/facultad</td>
+            <td>PUT /api/v1/facultad/{id}</td>
+            <td>DELETE /api/v1/facultad/{id}</td>
+          </tr>
+          <tr>
+            <th scope="row"> INVESTIGADOR</th>
+        
+            <td>GET /api/v1/investigadores</td>
+            <td>GET /api/v1/investigadore/{id}</td>
+            <td>POST /api/v1/investigador</td>
+            <td>PUT /api/v1/investigador/{id}</td>
+            <td>DELETE /api/v1/investigador/{id}</td>
+          </tr>
+          <tr>
+            <th scope="row"> RESERVA </th>
+            
+            <td>GET /api/v1/reservas</td>
+            <td>GET /api/v1/reserva/{id}</td>
+            <td>POST /api/v1/reserva</td>
+            <td>PUT /api/v1/reserva/{id}</td>
+            <td>DELETE /api/v1/reserva/{id}</td>
+          </tr>
+        </tbody>
+      </table> 
+       </small>`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-spring-1611-ta27/tree/main/TA27_4',
+        urlPG: '',
+      },
+    ],
+  },
+]
